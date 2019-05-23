840f3972ffdcd976bee67bd95ff6c0960d33e97d
kasiaqq <katarzynamajcherqq@gmail.com>
2019-05-23 12:31:04 +0200

    Changes for server

--- MVC_Management/Program.cs
+++ MVC_Management/Program.cs
@@ -19,6 +19,7 @@ namespace MVC_Management
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://35.198.98.72/mvc");
     }
 }
